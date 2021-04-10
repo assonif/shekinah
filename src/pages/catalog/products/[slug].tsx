f3986f4b2c4ae397bffe2102052756bf745a37ba
Fábio Furtado <fabioassoni06@gmail.com>
2021-04-10 18:27:25 -0300

    test

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,282 +1,286 @@
-import { useEffect, useState } from "react";
-import { GetStaticPaths, GetStaticProps } from "next";
-import { useRouter } from "next/router";
-
-import Layout from "@/components/Layout";
-import SEO from "@/components/SEO";
-import Button from "@/components/Button";
-import LoadingPage from "@/components/LoadingPage";
-
-import { useRecoilState, useRecoilValue } from "recoil";
-import { cartState } from "@/atoms";
-
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-import {
-  Container,
-  PhotoContainer,
-  MiniPhotoContainer,
-  InfoContainer,
-  SizeBox,
-  SaleContainer,
-} from "@/styles/pages/Products";
-import Colors from "@/styles/Colors";
-import Carroussel from "@/components/Carroussel";
-import Accordion from "@/components/Accordion";
-import { db } from "@/config/firebase";
-
-interface ProductProps {
-  id: number;
-  title: string;
-  cover_photo: string;
-  photos: string[];
-  brand: string;
-  price: string;
-  sale_price: string;
-  sizes: string[];
-  category_id: string;
-  slug: string;
-  quantity: number;
-  description: string;
+export default function Test() {
+  return <h1>test</h1>;
 }
 
-const Product = ({
-  id,
-  title,
-  category_id,
-  sizes,
-  brand,
-  cover_photo,
-  photos,
-  price,
-  sale_price,
-  slug,
-  quantity,
-  description,
-}: ProductProps) => {
-  const router = useRouter();
+// import { useEffect, useState } from "react";
+// import { GetStaticPaths, GetStaticProps } from "next";
+// import { useRouter } from "next/router";
 
-  const [selected, setSelected] = useState(1);
-  const [photo, setPhoto] = useState(cover_photo);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [selectedSize, setSelectedSize] = useState("");
-  const [hasError, setHasError] = useState(false);
+// import Layout from "@/components/Layout";
+// import SEO from "@/components/SEO";
+// import Button from "@/components/Button";
+// import LoadingPage from "@/components/LoadingPage";
 
-  const [cart, setCart] = useRecoilState(cartState);
+// import { useRecoilState, useRecoilValue } from "recoil";
+// import { cartState } from "@/atoms";
 
-  if (router.isFallback) {
-    return <LoadingPage />;
-  }
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
 
-  const notify = (value) =>
-    toast.success(value, {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
+// import {
+//   Container,
+//   PhotoContainer,
+//   MiniPhotoContainer,
+//   InfoContainer,
+//   SizeBox,
+//   SaleContainer,
+// } from "@/styles/pages/Products";
+// import Colors from "@/styles/Colors";
+// import Carroussel from "@/components/Carroussel";
+// import Accordion from "@/components/Accordion";
+// import { db } from "@/config/firebase";
 
-  const checkQuantity = (value) => {
-    if (value > quantity) {
-      return false;
-    }
-    return true;
-  };
+// interface ProductProps {
+//   id: number;
+//   title: string;
+//   cover_photo: string;
+//   photos: string[];
+//   brand: string;
+//   price: string;
+//   sale_price: string;
+//   sizes: string[];
+//   category_id: string;
+//   slug: string;
+//   quantity: number;
+//   description: string;
+// }
 
-  const updateValue = () => {
-    const copy = [...cart];
-    let index;
-    let copyItem;
+// const Product = ({
+//   id,
+//   title,
+//   category_id,
+//   sizes,
+//   brand,
+//   cover_photo,
+//   photos,
+//   price,
+//   sale_price,
+//   slug,
+//   quantity,
+//   description,
+// }: ProductProps) => {
+//   const router = useRouter();
 
-    copy.forEach((item, i) => {
-      if (item.id === id) {
-        index = i;
-        copyItem = item;
-      }
-    });
+//   const [selected, setSelected] = useState(1);
+//   const [photo, setPhoto] = useState(cover_photo);
+//   const [errorMessage, setErrorMessage] = useState("");
+//   const [selectedSize, setSelectedSize] = useState("");
+//   const [hasError, setHasError] = useState(false);
 
-    if (!checkQuantity(copyItem.quantity + 1)) {
-      setHasError(true);
-      setErrorMessage("VOCÊ JÁ ADICIONOU TODO NOSSO ESTOQUE EM SEU CARRINHO");
-      return;
-    }
+//   const [cart, setCart] = useRecoilState(cartState);
 
-    copyItem = { ...copyItem, quantity: copyItem.quantity + 1 };
+//   if (router.isFallback) {
+//     return <LoadingPage />;
+//   }
 
-    copy.splice(index, 1);
+//   const notify = (value) =>
+//     toast.success(value, {
+//       position: "top-right",
+//       autoClose: 5000,
+//       hideProgressBar: false,
+//       closeOnClick: true,
+//       pauseOnHover: true,
+//       draggable: true,
+//       progress: undefined,
+//     });
 
-    copy.splice(index, 0, copyItem);
+//   const checkQuantity = (value) => {
+//     if (value > quantity) {
+//       return false;
+//     }
+//     return true;
+//   };
 
-    setCart(copy);
+//   const updateValue = () => {
+//     const copy = [...cart];
+//     let index;
+//     let copyItem;
 
-    localStorage.setItem("cart", JSON.stringify(copy));
+//     copy.forEach((item, i) => {
+//       if (item.id === id) {
+//         index = i;
+//         copyItem = item;
+//       }
+//     });
 
-    notify("Produto atualizado no carrinho!");
-  };
+//     if (!checkQuantity(copyItem.quantity + 1)) {
+//       setHasError(true);
+//       setErrorMessage("VOCÊ JÁ ADICIONOU TODO NOSSO ESTOQUE EM SEU CARRINHO");
+//       return;
+//     }
 
-  const handleBuy = () => {
-    if (!selectedSize || selectedSize === "") {
-      setHasError(true);
-      setErrorMessage("POR FAVOR, SELECIONE UM TAMANHO.");
-      return;
-    }
-    let existsInCart = false;
-    let currentQuantity = 1;
+//     copyItem = { ...copyItem, quantity: copyItem.quantity + 1 };
 
-    cart.map((item) => {
-      if (item.id === id) {
-        updateValue();
+//     copy.splice(index, 1);
 
-        existsInCart = true;
-      }
-    });
+//     copy.splice(index, 0, copyItem);
 
-    if (!existsInCart) {
-      const copy = [
-        ...cart,
-        {
-          id,
-          title,
-          price: sale_price || price,
-          cover_photo,
-          quantity: currentQuantity,
-          size: selectedSize,
-          maxQuantity: quantity,
-        },
-      ];
-      setCart(copy);
-      localStorage.setItem("cart", JSON.stringify(copy));
-      notify("Produto adicionado ao carrinho!");
-    }
-  };
+//     setCart(copy);
 
-  const setAttributes = (value) => {
-    setSelected(value);
-    if (value > 0) {
-      setPhoto(value === 1 ? cover_photo : photos[value - 2]);
-    }
-  };
+//     localStorage.setItem("cart", JSON.stringify(copy));
 
-  const setSize = (value) => {
-    setSelectedSize(value.toUpperCase());
-    setHasError(false);
-  };
+//     notify("Produto atualizado no carrinho!");
+//   };
 
-  return (
-    <Layout>
-      <SEO title={title} />
-      <ToastContainer />
-      <Container>
-        <div>
-          <PhotoContainer>
-            <span>
-              <img src={photo || cover_photo} alt="foto de capa" />
-            </span>
+//   const handleBuy = () => {
+//     if (!selectedSize || selectedSize === "") {
+//       setHasError(true);
+//       setErrorMessage("POR FAVOR, SELECIONE UM TAMANHO.");
+//       return;
+//     }
+//     let existsInCart = false;
+//     let currentQuantity = 1;
 
-            <div>
-              <MiniPhotoContainer
-                onClick={() => {
-                  setAttributes(1);
-                }}
-                selected={selected}
-              >
-                <img src={cover_photo} alt="foto de capa" />
-              </MiniPhotoContainer>
-              {photos.length > 0 &&
-                photos.map((photo, index) => (
-                  <MiniPhotoContainer
-                    key={index}
-                    onClick={() => {
-                      setAttributes(index + 2);
-                    }}
-                    selected={selected}
-                  >
-                    <img src={photo} alt={`${title} foto ${index}`} />
-                  </MiniPhotoContainer>
-                ))}
-            </div>
-          </PhotoContainer>
-          <div>
-            <InfoContainer>
-              <p>{brand}</p>
-              <strong>{title.toUpperCase()}</strong>
-              <SaleContainer hasSale={sale_price !== ""}>
-                <strong>R$ {price}</strong>
-                <span>R$ {sale_price}</span>
-              </SaleContainer>
+//     cart.map((item) => {
+//       if (item.id === id) {
+//         updateValue();
 
-              <span>Tamanho: {selectedSize && <b>{selectedSize}</b>}</span>
-              <div>
-                {sizes.map((size, index) => (
-                  <SizeBox
-                    key={index}
-                    onClick={() => setSize(size.toUpperCase())}
-                    isSelected={selectedSize === size.toUpperCase()}
-                  >
-                    {size.toUpperCase()}
-                  </SizeBox>
-                ))}
-              </div>
-              <label>PEÇAS RESTANTES: {quantity}</label>
-            </InfoContainer>
-            {hasError && <span>{errorMessage}</span>}
-            <Button title="COMPRAR" color={Colors.blue_01} action={handleBuy} />
-          </div>
-        </div>
-        <Accordion title="DETALHES" content={description} />
-        <Carroussel category={category_id} title="PRODUTOS SIMILARES" />
-      </Container>
-    </Layout>
-  );
-};
+//         existsInCart = true;
+//       }
+//     });
 
-export default Product;
+//     if (!existsInCart) {
+//       const copy = [
+//         ...cart,
+//         {
+//           id,
+//           title,
+//           price: sale_price || price,
+//           cover_photo,
+//           quantity: currentQuantity,
+//           size: selectedSize,
+//           maxQuantity: quantity,
+//         },
+//       ];
+//       setCart(copy);
+//       localStorage.setItem("cart", JSON.stringify(copy));
+//       notify("Produto adicionado ao carrinho!");
+//     }
+//   };
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    paths: [],
-    fallback: true,
-  };
-};
+//   const setAttributes = (value) => {
+//     setSelected(value);
+//     if (value > 0) {
+//       setPhoto(value === 1 ? cover_photo : photos[value - 2]);
+//     }
+//   };
 
-export const getStaticProps: GetStaticProps<ProductProps> = async (context) => {
-  const { slug } = context.params;
+//   const setSize = (value) => {
+//     setSelectedSize(value.toUpperCase());
+//     setHasError(false);
+//   };
 
-  let data = [];
-  let error = {};
+//   return (
+//     <Layout>
+//       <SEO title={title} />
+//       <ToastContainer />
+//       <Container>
+//         <div>
+//           <PhotoContainer>
+//             <span>
+//               <img src={photo || cover_photo} alt="foto de capa" />
+//             </span>
 
-  await db
-    .collection("Products")
-    .where("slug", "==", slug)
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        data.push(doc.data());
-      });
-    })
-    .catch((e) => (error = e));
+//             <div>
+//               <MiniPhotoContainer
+//                 onClick={() => {
+//                   setAttributes(1);
+//                 }}
+//                 selected={selected}
+//               >
+//                 <img src={cover_photo} alt="foto de capa" />
+//               </MiniPhotoContainer>
+//               {photos.length > 0 &&
+//                 photos.map((photo, index) => (
+//                   <MiniPhotoContainer
+//                     key={index}
+//                     onClick={() => {
+//                       setAttributes(index + 2);
+//                     }}
+//                     selected={selected}
+//                   >
+//                     <img src={photo} alt={`${title} foto ${index}`} />
+//                   </MiniPhotoContainer>
+//                 ))}
+//             </div>
+//           </PhotoContainer>
+//           <div>
+//             <InfoContainer>
+//               <p>{brand}</p>
+//               <strong>{title.toUpperCase()}</strong>
+//               <SaleContainer hasSale={sale_price !== ""}>
+//                 <strong>R$ {price}</strong>
+//                 <span>R$ {sale_price}</span>
+//               </SaleContainer>
 
-  const products: ProductProps = data[0];
+//               <span>Tamanho: {selectedSize && <b>{selectedSize}</b>}</span>
+//               <div>
+//                 {sizes.map((size, index) => (
+//                   <SizeBox
+//                     key={index}
+//                     onClick={() => setSize(size.toUpperCase())}
+//                     isSelected={selectedSize === size.toUpperCase()}
+//                   >
+//                     {size.toUpperCase()}
+//                   </SizeBox>
+//                 ))}
+//               </div>
+//               <label>PEÇAS RESTANTES: {quantity}</label>
+//             </InfoContainer>
+//             {hasError && <span>{errorMessage}</span>}
+//             <Button title="COMPRAR" color={Colors.blue_01} action={handleBuy} />
+//           </div>
+//         </div>
+//         <Accordion title="DETALHES" content={description} />
+//         <Carroussel category={category_id} title="PRODUTOS SIMILARES" />
+//       </Container>
+//     </Layout>
+//   );
+// };
 
-  return {
-    props: {
-      id: products.id,
-      title: products.title,
-      cover_photo: products.cover_photo,
-      photos: products.photos,
-      brand: products.brand,
-      price: products.price,
-      sale_price: products.sale_price,
-      sizes: products.sizes,
-      category_id: products.category_id,
-      slug: products.slug,
-      quantity: products.quantity,
-      description: products.description,
-    },
-    revalidate: 60,
-  };
-};
+// export default Product;
+
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   return {
+//     paths: [],
+//     fallback: true,
+//   };
+// };
+
+// export const getStaticProps: GetStaticProps<ProductProps> = async (context) => {
+//   const { slug } = context.params;
+
+//   let data = [];
+//   let error = {};
+
+//   await db
+//     .collection("Products")
+//     .where("slug", "==", slug)
+//     .get()
+//     .then((querySnapshot) => {
+//       querySnapshot.forEach((doc) => {
+//         data.push(doc.data());
+//       });
+//     })
+//     .catch((e) => (error = e));
+
+//   const products: ProductProps = data[0];
+
+//   return {
+//     props: {
+//       id: products.id,
+//       title: products.title,
+//       cover_photo: products.cover_photo,
+//       photos: products.photos,
+//       brand: products.brand,
+//       price: products.price,
+//       sale_price: products.sale_price,
+//       sizes: products.sizes,
+//       category_id: products.category_id,
+//       slug: products.slug,
+//       quantity: products.quantity,
+//       description: products.description,
+//     },
+//     revalidate: 60,
+//   };
+// };
